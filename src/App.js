962904c34f4d3d3d27e55e6abd55a34611c3d21e
bgopikrishna <gopiwayz@gmail.com>
2019-03-15 21:44:46 +0530

    added home page loader

--- src/App.js
+++ src/App.js
@@ -5,23 +5,35 @@ import { Switch, Route } from "react-router-dom";
 import SearchPage from "./pages/Searchpage";
 import NoPage404 from "./pages/NoPage404";
 import MovieInfoPage from "./pages/MovieInfoPage";
+import Loader from "./components/Loader";
 
 class App extends Component {
+  state = {
+    isLoading: true
+  };
+  componentDidMount() {
+    this.setState({ isLoading: false });
+  }
   render() {
-    return (
-      <div className="app">
-        <Navbar />
+    const { isLoading } = this.state;
+    if (!isLoading) {
+      return (
+        <div className="app">
+          <Navbar />
 
-        <Switch>
-          <Route exact path="/" component={SearchPage} />
-          <Route exact path="/search" component={SearchPage} />
+          <Switch>
+            <Route exact path="/" component={SearchPage} />
+            <Route exact path="/search" component={SearchPage} />
 
-          <Route path="/404" component={NoPage404} />
-          <Route path="/account" component={NoPage404} />
-          <Route path="/movie/:movie_Id" component={MovieInfoPage} />
-        </Switch>
-      </div>
-    );
+            <Route path="/404" component={NoPage404} />
+            <Route path="/account" component={NoPage404} />
+            <Route path="/movie/:movie_Id" component={MovieInfoPage} />
+          </Switch>
+        </div>
+      );
+    } else {
+      return <Loader />;
+    }
   }
 }
 
