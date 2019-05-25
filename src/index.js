85254a459a88755ac4f13fb8cf667f8274401ad5
Gopi Krishna <gopiwayz@gmail.com>
2019-05-25 23:37:59 +0530

    Enabled CRA service workder and added manifest.json

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ store.firebaseAuthIsReady.then(() =>
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 
 if (module.hot) {
   module.hot.accept();
